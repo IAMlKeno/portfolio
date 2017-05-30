9d8d743c314cbf929b807f96c30dbc9c9c8f835c
Elkeno Jones <elkeno_q.jones@hotmail.com>
2017-05-30 19:07:35 -0300

    changed database connection

--- phpReadWriteExample/veggiesDatabaseConnection.php
+++ phpReadWriteExample/veggiesDatabaseConnection.php
@@ -8,7 +8,7 @@
  * *****************************************************************************************************************
  */
 
-$db = new mysqli('localhost', 'ejones', 'Test1234', 'veggies');
+$db = new mysqli('mgs0iaapcj3p9srz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'pcgw9sbez1mj6h3v', 'm7cils8i7quwxekk', 'veggies');
 
 if($db->connect_errno){
     echo "<p>The was an error connecting to the database. Please try again later.</p>";
