6fae2d8f445c5f75ddbe05855cb7cdd4f915e931
Elkeno Jones <elkeno_q.jones@hotmail.com>
2017-05-30 17:52:59 -0300

    added directory page
    added directory page

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Elkeno
+ * Date: 2017-05-30
+ * Time: 5:44 PM
+ */
+
+$title = "directory";
+?>
+<!DocType html>
+<html>
+<head>
+    <title>Directory</title>
+</head>
+<body>
+    <ul>
+        <li>Example 1</li>
+        <li>Example 2</li>
+    </ul>
+</body>
+</html>
