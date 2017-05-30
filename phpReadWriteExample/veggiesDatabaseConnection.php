a441644a520aa1cdc5dde2bdfcf1a49f0cfda1a4
Elkeno Jones <elkeno_q.jones@hotmail.com>
2017-05-30 19:10:40 -0300

    Update veggiesDatabaseConnection.php

--- phpReadWriteExample/veggiesDatabaseConnection.php
+++ phpReadWriteExample/veggiesDatabaseConnection.php
@@ -8,7 +8,7 @@
  * *****************************************************************************************************************
  */
 
-$db = new mysqli('mgs0iaapcj3p9srz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'pcgw9sbez1mj6h3v', 'm7cils8i7quwxekk', 'veggies');
+$db = new mysqli('mgs0iaapcj3p9srz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'pcgw9sbez1mj6h3v', 'm7cils8i7quwxekk', 'd4sqila9g6x7qxou');
 
 if($db->connect_errno){
     echo "<p>The was an error connecting to the database. Please try again later.</p>";
