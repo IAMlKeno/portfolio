0c5931c3d8c3985ad0767bb56973afb1d6cc3798
iamlkeno <elkeno_q.jones@hotmail.com>
2022-02-14 19:28:32 -0400

    add in a xml file to grab the examples

---
+++ list.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * List projects based on directory layout.
+ * User: Elkeno
+ * Date: 2022-02-14
+ */
+namespace portfolio;
+$title = "List";
+
+require("Util/model/Project.php");
+require("Util/fragments/head.php");
+?>
+<body>
+  <div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-drawer mdl-layout--fixed-header">
+    <?php require("Util/fragments/menu.php"); ?>
+    <main class="mdl-layout__content mdl-color--grey-100" style="min-height:600px">
+      <div class="mdl-grid demo-content">
+        <?php
+          // libxml_use_internal_errors(true);
+          $projectXml = simplexml_load_file(__DIR__ . '/resources/projects.xml');
+          // if ($projectXml === false) {
+          //   foreach(libxml_get_errors() as $error) {
+          //     echo "<br>", $error->message;
+          //   }
+          //   die();
+          // }
+
+          $projects = $projectXml->project;
+          if (count($projects) > 0) {
+            foreach ($projects as $project) {
+              include("Util/fragments/mdlCard.php");
+            }
+          }
+          else {
+            include ("Util/fragments/noResultsCard.php");
+          }
+          ?>
+          </div>
+        </main>
+      </div>
+<?php include ("Util/fragments/footer.php");
