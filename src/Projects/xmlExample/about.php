6903eccce95648c174a09c8cb1b60946b2c0043d
iamlkeno <elkeno_q.jones@hotmail.com>
2022-02-14 20:54:15 -0400

    Fixing up the xml example

--- src/Projects/xmlExample/about.php
+++ src/Projects/xmlExample/about.php
@@ -9,49 +9,47 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>XML Example - About</title>
-    <link rel="stylesheet" href="stylesheets/xmlStyle.css"/>
-    <link rel="stylesheet" href="stylesheets/menuStyle.css"/>
+  <title>XML Example - About</title>
+  <link rel="stylesheet" href="stylesheets/xmlStyle.css"/>
+  <link rel="stylesheet" href="stylesheets/menuStyle.css"/>
 </head>
 <body>
-    <div id="container" style="width: 65%; margin: auto;">
-        <div class="navBar">
-            <ul>
-                <li>
-                    <a href="index.php">
-                        Forecast PHP
-                    </a>
-                </li>
-                <li>
-                    <a href="forecast.xml">
-                        Forecast XSLT
-                    </a>
-                </li>
-                <li>
-                    <a href="part1/audioReceiverXML">
-                        Audio Receiver XSLT
-                    </a>
-                </li>
-                <li>
-                    <a href="about.php">
-                        About Project
-                    </a>
-                </li>
-                <li>
-                    <a href="../index.php">
-                        Back to Portfolio
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <p>
-            This is a combination of PHP, XML and XSLT. This project demonstrates using PHP to read from an arbitrary XML
-            document. It also demonstrates using XSLT to properly display the XML files. There are two XML documents that
-            are used to show this.
-        </p>
-        <p>
-            This project was created by Elkeno Jones during his time at Holland College.
-        </p>
+  <div id="container">
+    <div class="navBar">
+      <ul>
+        <li>
+          <a href="index.php">
+            Forecast PHP
+          </a>
+        </li>
+        <li>
+          <a href="forecast.xml">
+            Forecast XSLT
+          </a>
+        </li>
+        <li>
+          <a href="part1/audioReceiverXML">
+            Audio Receiver XSLT
+          </a>
+        </li>
+        <li>
+          <a href="about.php">
+            About Project
+          </a>
+        </li>
+        <li>
+          <a href="../../../index.php">
+            Back to Portfolio
+          </a>
+        </li>
+      </ul>
     </div>
+    <p>
+      This is a combination of PHP, XML and XSLT. This project demonstrates using PHP to read from an arbitrary XML document. It also demonstrates using XSLT to properly display the XML files. There are two XML documents that are used to show this.
+    </p>
+    <p>
+      This project was created by Elkeno Jones during his time at Holland College.
+    </p>
+  </div>
 </body>
 </html>
--- src/Projects/xmlExample/index.php
+++ src/Projects/xmlExample/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @author: Elkeno Jones_ejones109029
  * Date: 2016-11-06
@@ -8,100 +9,86 @@
  * the index page for the xml web page. The user can view the xsl property by going directly to the forecast.xml page,
  */
 ?>
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>XML Example - PHP</title>
-        <link rel="stylesheet" href="stylesheets/xmlStyle.css"/>
-        <link rel="stylesheet" href="stylesheets/menuStyle.css"/>
-    </head>
-    <body>
-    <div class="navBar">
-        <ul>
-            <li>
-                <a href="index.php">
-                    Forecast PHP
-                </a>
-            </li>
-            <li>
-                <a href="forecast.xml">
-                    Forecast XSLT
-                </a>
-            </li>
-            <li>
-                <a href="part1/audioReceiverXML">
-                    Audio Receiver XSLT
-                </a>
-            </li>
-            <li>
-                <a href="about.php">
-                    About Project
-                </a>
-            </li>
-            <li>
-                <a href="../index.php">
-                    Back to Portfolio
-                </a>
-            </li>
-        </ul>
-    </div>
-    <div id="container">
-        <?php
-            // initialize variables (xml object, averages and a counter)
-            $xml = simplexml_load_file("forecast.xml");
-            $location = $xml->location;
-            $daily = $xml->daily;
-            $averageLow = 0;
-            $averageHigh = 0;
-            $counter = 0;
+  <head>
+    <title>XML Example - PHP</title>
+    <link rel="stylesheet" href="stylesheets/xmlStyle.css"/>
+    <link rel="stylesheet" href="stylesheets/menuStyle.css"/>
+  </head>
+  <body>
+  <div class="navBar">
+    <ul>
+      <li>
+        <a href="index.php">Forecast PHP</a>
+      </li>
+      <li>
+        <a href="forecast.xml">Forecast XSLT</a>
+      </li>
+      <li>
+        <a href="part1/audioReceiverXML">Audio Receiver XSLT</a>
+      </li>
+      <li>
+        <a href="./about.php">About Project</a>
+      </li>
+      <li>
+        <a href="../../../index.php">Back to Portfolio</a>
+      </li>
+    </ul>
+  </div>
+  <div id="container">
+  <?php
+    // Initialize variables (xml object, averages and a counter).
+    $xml = simplexml_load_file("forecast.xml");
+    $location = $xml->location;
+    $daily = $xml->daily;
+    $averageLow = 0;
+    $averageHigh = 0;
+    $counter = 0;
 
-            // this section read the location in the xml object
-            echo '    <span class="location">Forecast for: '. $location->city. ', '. $location->province.
-                ', '. $location->country.'</span>
-            <br />';
-            echo '
-            <span class="dateIssued">issued at '.$location->issued.'</span>
-            <br />';
-            echo '
-            <span class="heading">Five day forecast</span>
-            <br />';
-            echo '
-            <span class="degrees">Degrees in '. $location->degrees .'</span>';
-            echo '
-            <table class="forecast">
-                <tbody>
-                    <tr>';
-
-            // using this loop, is will iterate through each day
-            foreach($daily->day as $day)
-            {
-                ++$counter;
-                $averageHigh += (int) $day->high;
-                $averageLow += (int) $day->low;
-
-                echo '
-                        <td>'.
-                            $day->date.'
-                            <br /><img src="images/'.$day->condition.'.jpg" />
-                            <br />
-                            High: '.$day->high.'&deg;
-                            <br/>
-                            Low: '.$day->low.'&deg;
-                            <hr />'.
-                            $day->description.'
-                        </td>';
-            }
-            // calculation for the averages of the high and low
-            $averageHigh = $averageHigh / $counter;
-            $averageLow = $averageLow / $counter;
+    // This section read the location in the xml object.
+    echo '    <span class="location">Forecast for: '. $location->city. ', '. $location->province. ', '. $location->country.'</span><br />';
+    echo '
+      <span class="dateIssued">issued at '.$location->issued.'</span>
+      <br />';
+    echo '
+      <span class="heading">Five day forecast</span>
+      <br />';
+    echo '
+      <span class="degrees">Degrees in '. $location->degrees .'</span>';
+    echo '
+      <table class="forecast">
+        <tbody>
+          <tr>';
+            // Using this loop, it will iterate through each day.
+          foreach ($daily->day as $day) {
+            ++$counter;
+            $averageHigh += (int) $day->high;
+            $averageLow += (int) $day->low;
 
             echo '
-                    </tr>
-                </tbody>
-            </table>
-            <h4>Average High: '. $averageHigh.'&deg;</h4>
-            <h4>Average Low: '. $averageLow.'&deg;</h4>';
+              <td>'.
+                $day->date . '<br /><img src="images/' . $day->condition . '.jpg" />
+                <br />
+                High: ' . $day->high . '&deg;
+                <br/>
+                Low: ' . $day->low . '&deg;
+                <hr />' .
+                $day->description .
+              '</td>';
+          }
+          // Calculation for the averages of the high and low.
+          $averageHigh = $averageHigh / $counter;
+          $averageLow = $averageLow / $counter;
+
+          echo '
+            </tr>
+          </tbody>
+        </table>
+        <h4>Average High: ' . $averageHigh . '&deg;</h4>
+        <h4>Average Low: ' . $averageLow . '&deg;</h4>';
         ?>
     </div>
-    </body>
+  </body>
 </html>
